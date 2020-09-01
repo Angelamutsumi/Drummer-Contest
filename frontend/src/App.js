7cf83c75d78bb889cb4098c1557d338df49568a4
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-09-01 18:09:21 -0300

    Integração com backend e implementa setInterval

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,58 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
 
 export default class App extends Component {
-  render() {
-    return <ProjetoBase />;
-  }
+	constructor() {
+		super();
+
+		this.state = {
+			candidates: []
+		};
+
+		this.interval = null;
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			fetch('http://localhost:8080/votes')
+				.then((res) => {
+					return res.json();
+				})
+				.then((json) => {
+					this.setState({
+						candidates: json.candidates
+					});
+				});
+		}, 1000);
+	}
+	render() {
+		const { candidates } = this.state;
+		if (candidates.length === 0) {
+			return (
+				<div className="preloader-wrapper big active">
+					<div className="spinner-layer spinner-blue-only">
+						<div className="circle-clipper left">
+							<div className="circle" />
+						</div>
+						<div className="gap-patch">
+							<div className="circle" />
+						</div>
+						<div className="circle-clipper right">
+							<div className="circle" />
+						</div>
+					</div>
+				</div>
+			);
+		}
+		return (
+			<div>
+				{candidates.map(({ id, name, votes }) => {
+					return (
+						<p key={id}>
+							{name} - {votes}
+						</p>
+					);
+				})}
+			</div>
+		);
+	}
 }
