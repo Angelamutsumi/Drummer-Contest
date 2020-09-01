edbf9869979827a39a393ccda0361033063862c4
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-09-01 19:05:14 -0300

    Cria Position e Picture

--- frontend/src/components/Candidate.js
+++ frontend/src/components/Candidate.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Position from './Position';
+import Picture from './Picture';
 
-export default function Candidate({ candidate }) {
-	const { name, votes, percentage } = candidate;
+export default function Candidate({ candidate, position }) {
+	const { id, name, votes, percentage } = candidate;
+
+	const imageSource = `${id}.jpg`;
 	return (
 		<div>
+			<Position>{position}</Position>
+			<Picture imageSource={imageSource} description={name} />
 			{name} - {votes}
 		</div>
 	);
