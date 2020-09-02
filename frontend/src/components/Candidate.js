d8680d83afdefe0c59f096720cb5b758f686c2e4
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-09-02 16:20:26 -0300

    Cria Popularity

--- frontend/src/components/Candidate.js
+++ frontend/src/components/Candidate.js
@@ -5,9 +5,10 @@ import Info from './Info';
 import Name from './Name';
 import Votes from './Votes';
 import Percentage from './Percentage';
+import Popularity from './Popularity';
 
 export default function Candidate({ candidate, position }) {
-	const { id, name, votes, percentage } = candidate;
+	const { id, name, votes, percentage, popularity } = candidate;
 
 	const imageSource = `${id}.jpg`;
 	return (
@@ -18,6 +19,7 @@ export default function Candidate({ candidate, position }) {
 				<Name>{name}</Name>
 				<Votes>{votes}</Votes>
 				<Percentage>{percentage}</Percentage>
+				<Popularity value={popularity} />
 			</Info>
 		</div>
 	);
